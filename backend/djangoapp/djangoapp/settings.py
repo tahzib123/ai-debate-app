0dfd62c3fa93dd2257d3cc94a9907d1a0c2e60ef
tahzib123 <tahzibsunesara1@gmail.com>
2025-12-16 21:38:44 -0600

    update backend requirements

--- backend/djangoapp/djangoapp/settings.py
+++ backend/djangoapp/djangoapp/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-m^t$ysj1_e##xp@$c2irypmds8d7(mg1l3pceiu!9c(%=@4+0!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'whitenoise.runserver_nostatic',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
@@ -137,6 +138,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URLS = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field

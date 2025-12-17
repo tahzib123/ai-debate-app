0ef0ec515746080eb0e059b677acce5a6c9a5549
tahzib123 <tahzibsunesara1@gmail.com>
2025-12-16 21:47:03 -0600

    update backend requirements

--- backend/djangoapp/djangoapp/urls.py
+++ backend/djangoapp/djangoapp/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include, path
 from django.conf.urls.static import static
-from . import settings
+from django.conf import settings
 
 
 urlpatterns = [
